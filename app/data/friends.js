7feb4d3442bb11ff476532895ebda8a35a7a4780
Temme <ddesantiago0@gmail.com>
2019-05-31 16:26:31 -0500

    update of directory and files

---
+++ app/data/friends.js
@@ -0,0 +1,21 @@
+var tableArray = [
+    {
+    name: "Ahmed",
+    photo: "https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAq7AAAAJDAwYzI4NTQ4LWYwZWUtNGFkYS1hNTYwLTZjYzkwY2ViZDA3OA.jpg",
+    scores: [
+    5,
+    1,
+    4,
+    4,
+    5,
+    1,
+    2,
+    5,
+    4,
+    1
+    ]
+    }
+  ]
+  
+  // Note how we export the array. This makes it accessible to other files using require.
+  module.exports = tableArray;
